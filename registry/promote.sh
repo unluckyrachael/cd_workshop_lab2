ffce17c1c0cb71d98c888f052fc0ccec517ac1df
gamename <tennis.n.smith@gmail.com>
2018-04-27 16:00:48 -0500

    logged the tag we found. Add more doc to explain use.

--- registry/promote.sh
+++ registry/promote.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #
-# This will find the latest tag and then set it on s3
+# This will find the latest tag and then set it on s3. The training instances will pick up the change and then
+# install the latest docker image based on that tag.
 #
 #
 set -e
 
 tag=$(curl http://registry.roundtower.io:5000/v2/${1}/lab1/tags/list | jq -r .tags[] | sort | tail -1)
+echo "tag is ${tag}"
 echo lab1:${tag} > path.txt
 aws s3 cp path.txt "s3://cd-workshop/${1}/"
 
