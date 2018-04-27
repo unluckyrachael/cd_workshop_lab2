8ac5c61cfbbe0380330dd85a405ecea6dec526ba
gamename <tennis.n.smith@gmail.com>
2018-04-27 15:57:49 -0500

    need to sort to get the latest

--- registry/promote.sh
+++ registry/promote.sh
@@ -5,7 +5,7 @@
 #
 set -e
 
-tag=$(curl http://registry.roundtower.io:5000/v2/${1}/lab1/tags/list | jq -r .tags[] | head -1)
+tag=$(curl http://registry.roundtower.io:5000/v2/${1}/lab1/tags/list | jq -r .tags[] | sort | tail -1)
 echo lab1:${tag} > path.txt
 aws s3 cp path.txt "s3://cd-workshop/${1}/"
 
