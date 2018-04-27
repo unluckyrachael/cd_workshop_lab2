f59a728572fb1cc8de4f4f2397f5cb02436872da
gamename <tennis.n.smith@gmail.com>
2018-04-27 15:51:33 -0500

    doco

--- registry/promote.sh
+++ registry/promote.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#
+# This will find the latest tag and then set it on s3
+#
+#
 set -e
 
 tag=$(curl http://registry.roundtower.io:5000/v2/${1}/lab1/tags/list | jq -r .tags[] | head -1)
 echo lab1:${tag} > path.txt
 aws s3 cp path.txt "s3://cd-workshop/${1}/"
+
