a35948e327d939386d01b4aa53537e07b2a810af
gamename <tennis.n.smith@gmail.com>
2018-04-30 14:13:58 -0500

    document better what we are doing

--- registry/promote.sh
+++ registry/promote.sh
@@ -6,8 +6,15 @@
 #
 set -e
 
+# Get the latest tag from the registry
 tag=$(curl http://registry.roundtower.io:5000/v2/${1}/lab1/tags/list | jq -r .tags[] | sort | tail -1)
+
+# Log what we found
 echo "tag is ${tag}"
+
+# Append that number to our "Lab1" string and push it to a file
 echo lab1:${tag} > path.txt
+
+# Copy that file to s3
 aws s3 cp path.txt "s3://cd-workshop/${1}/"
 
